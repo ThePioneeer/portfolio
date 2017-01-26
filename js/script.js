6d5fe79aef9a0ed0b111a43b3b9a0325514f44da
ThePioneeer <uko_1@hotmail.com>
2017-01-26 20:20:58 +0200

    added scrollspy

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
  $(document).ready(function () {
+     //Add smooth scrolling to links
      $("a").on("click", function(event) {
          if(this.hash !== "") {
              event.preventDefault();
 
              var hash = this.hash;
 
+             // Using jQuery's animate() method to add smooth page scroll
              $("html, body").animate({
                  scrollTop: $(hash).offset().top
              }, 800, function() {
