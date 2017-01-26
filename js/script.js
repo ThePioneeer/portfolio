3b4a79c4afbb19f3f0570263a254db957b83cf1f
ThePioneeer <uko_1@hotmail.com>
2017-01-26 20:10:17 +0200

    smooth link scrolling

--- js/script.js
+++ js/script.js
@@ -1,37 +1,15 @@
  $(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('li').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-            
-        var target = this.hash;
-        $target = $(target);
-        $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
-        }, 800, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-    
-function onScroll(event){
-    var scrollPosition = $(document).scrollTop();
-    $('nav li').each(function () {
-    var currentLink = $(this);
-    var refElement = $(currentLink.attr("href"));
-        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-            $('.navbar-nav ul li').removeClass("active");
-            currentLink.addClass("active");
-        }
-        else{
-            currentLink.removeClass("active");
-        }
-    });
- }
+     $("a").on("click", function(event) {
+         if(this.hash !== "") {
+             event.preventDefault();
+
+             var hash = this.hash;
+
+             $("html, body").animate({
+                 scrollTop: $(hash).offset().top
+             }, 800, function() {
+                 window.location.hash = hash;
+             });
+         }
+     })
+ });
